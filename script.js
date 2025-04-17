9cee1a2c3f949227f8d9732dd80793770d2df09b
Khem Raj Budhathoki <khemverse@gmail.com>
2025-04-17 12:50:35 +0545

    Revert Changes

--- script.js
+++ script.js
@@ -7,22 +7,28 @@ document.addEventListener("DOMContentLoaded", function () {
 })();
 
 function notifyReception() {
+  const bellSound = document.getElementById("bellSound");
   const notification = document.getElementById("notification");
   const errorNotification = document.getElementById("errorNotification");
 
+
   emailjs
+    //Replace with your user_service-ID and templateID
     .send("service_n5uf9wr", "template_kw277jr", {
       visitor_name: "A guest",
       message: "Someone clicked the reception bell!",
     })
     .then(
-      function () {
+      function (response) {
+        console.log("Success:", response);
+
+        // Show success notification
         gsap.killTweensOf(notification);
         gsap.set(notification, {
           opacity: 0,
           y: "-20%",
           backgroundColor: "var(--success-color)",
-          boxShadow: "0 6px 15px rgba(16, 185, 129, 0.3)",
+          boxShadow: "0 6px 15px rgba(16, 185, 129, 0.3)", // green shadow
         });
         gsap.to(notification, {
           duration: 0.4,
@@ -31,6 +37,7 @@ function notifyReception() {
           display: "flex",
           ease: "power3.out",
         });
+
         setTimeout(() => {
           gsap.to(notification, {
             duration: 5,
@@ -40,13 +47,16 @@ function notifyReception() {
           });
         }, 300);
       },
-      function () {
+      function (error) {
+        console.error("Error:", error);
+
+        // Show error notification with red background and red shadow
         gsap.killTweensOf(errorNotification);
         gsap.set(errorNotification, {
           opacity: 0,
           y: "-20%",
           backgroundColor: "var(--error-color)",
-          boxShadow: "0 6px 15px #ff1d1d",
+          boxShadow: "0 6px 15px #ff1d1d", // red shadow
         });
         gsap.to(errorNotification, {
           duration: 0.4,
@@ -55,6 +65,7 @@ function notifyReception() {
           display: "flex",
           ease: "power3.out",
         });
+
         setTimeout(() => {
           gsap.to(errorNotification, {
             duration: 0.3,
